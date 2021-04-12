eecbb0c747aafc286fcd6c437e469a0f3e0f5435
mxie <836728902@qq.com>
2021-04-12 22:39:08 +0300

    sqlLite and tkinter

--- tkinter_project/script2.py
+++ tkinter_project/script2.py
@@ -3,7 +3,13 @@ window = Tk()
 
 
 def kg_convert():
-    print(kg_value.get())
+    input = kg_value.get()
+    gram = float(input) * 1000
+    t1.insert(END, gram)
+    pound = float(input) * 2.20462
+    t2.insert(END, pound)
+    ounce = float(input) * 35.274
+    t3.insert(END, ounce)
 
     #miles = float(kg_value.get())*1.6
     #t1.insert(END, miles)
@@ -19,6 +25,19 @@ b1 = Button(window, text="Convert", command=kg_convert)
 b1.grid(row=0, column=2)
 
 #next line is composed of 3 Text components
+labe_gram = Label(window, text="gram")
+labe_gram.grid(row=1, column=0)
+t1 = Text(window, height=2, width=20)
+t1.grid(row=1, column=1)
 
+labe_pound = Label(window, text="pound")
+labe_pound.grid(row=1, column=2)
+t2 = Text(window, height=2, width=20)
+t2.grid(row=1, column=3)
+
+labe_ounce = Label(window, text="ounce")
+labe_ounce.grid(row=1, column=4)
+t3 = Text(window, height=2, width=20)
+t3.grid(row=1, column=5)
 
 window.mainloop()
