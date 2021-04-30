92a958b8ea4cb0c5dadbb2dd1c8b4e6f83b650ac
mxie <836728902@qq.com>
2021-04-30 20:57:50 +0300

    sign up

--- kivy_app/main.py
+++ kivy_app/main.py
@@ -3,3 +3,16 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen 
 
 Builder.load_file('design.kv')
+
+class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current="sign_up_screen"
+class SignUpScreen(Screen):
+    pass
+class RootWidget(ScreenManager):
+    pass
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+if __name__=="__main__":
+    MainApp().run()
