0193ebb2b7f6b13b6509bb12cccb8e57a00f1e2e
mxie <836728902@qq.com>
2021-05-01 12:09:48 +0300

    kivy sign up

--- kivy_app/main.py
+++ kivy_app/main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen 
+import json 
+from datetime import datetime
 
 Builder.load_file('design.kv')
 
@@ -8,7 +10,18 @@ class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current="sign_up_screen"
 class SignUpScreen(Screen):
-    pass
+    def add_user(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+        users[uname] = {'username':uname, 'password': pword,
+        'created':datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+        self.manager.transition.direction="right"
+        self.manager.current="sign_up_screen_success"
+class SignUpScreenSuccess(Screen):
+    def back_to_login_in(self):
+        self.manager.current="login_screen"
 class RootWidget(ScreenManager):
     pass
 class MainApp(App):
